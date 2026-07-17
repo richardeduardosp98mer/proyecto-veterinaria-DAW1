8be90854550b00119b37b7eeccd1ee29532731f0
richardeduardosp98mer <richardeduardosp98@gmail.com>
2026-07-16 23:26:10 -0500

    Creando WebVeterinariaApplication

---
+++ src/main/java/web/veterinaria/WebVeterinariaApplication.java
@@ -0,0 +1,11 @@
+package web.veterinaria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebVeterinariaApplication {
+    public static void main(String[] args){
+        SpringApplication.run(WebVeterinariaApplication.class, args);
+    }
+}
