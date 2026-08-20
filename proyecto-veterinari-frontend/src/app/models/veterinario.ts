10673f0efb88d19e4b3ad825084cdffe21e0a336
sevillanodev <sevillanogbo@gmail.com>
2026-08-19 21:44:24 -0500

    frontend/models-services/veterinario

--- proyecto-veterinari-frontend/src/app/models/veterinario.ts
+++ proyecto-veterinari-frontend/src/app/models/veterinario.ts
@@ -1,1 +1,25 @@
-export interface Veterinario {}
+export interface VeterinarioResponse {
+  idVeterinario: number;
+  nombre: string;
+  apellido: string;
+  correo: string;
+  celular: string;
+  especialidad: string;
+  numeroColegiatura: string;
+}
+
+export interface RegistroVeterinarioRequest {
+  nombre: string;
+  apellido: string;
+  correo: string;
+  clave: string;
+  celular: string;
+  especialidad: string;
+  numeroColegiatura: string;
+}
+
+export interface ActualizarVeterinarioRequest {
+  nombre: string;
+  apellido: string;
+  celular: string;
+}
--- proyecto-veterinari-frontend/src/app/services/veterinario.ts
+++ proyecto-veterinari-frontend/src/app/services/veterinario.ts
@@ -1,4 +1,40 @@
-import { Service } from '@angular/core';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
+import { ActualizarVeterinarioRequest, RegistroVeterinarioRequest, VeterinarioResponse } from '../models/veterinario';
 
-@Service()
-export class Veterinario {}
+@Injectable({ providedIn: 'root' })
+export class VeterinarioService {
+  private baseUrl = `${environment.apiUrl}/veterinarios`;
+
+  constructor(private http: HttpClient) {}
+
+  registrar(veterinario: RegistroVeterinarioRequest): Observable<VeterinarioResponse> {
+    return this.http.post<VeterinarioResponse>(`${this.baseUrl}/registro`, veterinario);
+  }
+
+  listar(): Observable<VeterinarioResponse[]> {
+    return this.http.get<VeterinarioResponse[]>(this.baseUrl);
+  }
+
+  obtenerPorId(id: number): Observable<VeterinarioResponse> {
+    return this.http.get<VeterinarioResponse>(`${this.baseUrl}/${id}`);
+  }
+
+  buscarPorEspecialidad(especialidad: string): Observable<VeterinarioResponse[]> {
+    return this.http.get<VeterinarioResponse[]>(`${this.baseUrl}/buscar`, { params: { especialidad } });
+  }
+
+  buscarPorColegiatura(numeroColegiatura: string): Observable<VeterinarioResponse> {
+    return this.http.get<VeterinarioResponse>(`${this.baseUrl}/colegiatura/${numeroColegiatura}`);
+  }
+
+  actualizar(id: number, veterinario: ActualizarVeterinarioRequest): Observable<VeterinarioResponse> {
+    return this.http.put<VeterinarioResponse>(`${this.baseUrl}/${id}`, veterinario);
+  }
+
+  eliminar(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.baseUrl}/${id}`);
+  }
+}
